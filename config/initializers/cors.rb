621cd29d3390ace3d6d6285b22928f829343a567
fhs42724 <abitzan.mmt-b2018@fh-salzburg.ac.at>
2020-04-29 17:16:15 +0200

    Make it accessible

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     end
    
     allow do
-      origins 'localhost:3000', '127.0.0.1:3000', 'https://my-frontend.org'
+      origins 'localhost:3000', '127.0.0.1:3000', 'https://iou-andreas.herokuapp.com'
       resource '/api/v1/*',
         methods: %i(get post put patch delete options head)
     end
