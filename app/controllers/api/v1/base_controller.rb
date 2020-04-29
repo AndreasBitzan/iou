f2ed78c522b4bfd3cf72c5eaf7d34bf8b3d68f5f
fhs42724 <abitzan.mmt-b2018@fh-salzburg.ac.at>
2020-04-29 15:36:43 +0200

    Make it accessble

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,2 +1,3 @@
 class Api::V1::BaseController < ActionController::API
+    skip_before_action :verify_authenticity_token
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::UsersController < Api::V1::BaseController
-  skip_before_action :verify_authenticity_token
-
+  
     def index
       users = User.all
    
