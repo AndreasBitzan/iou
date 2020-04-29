646b01149fe781f3c6a8cf452a55b21e2173aa5f
fhs42724 <abitzan.mmt-b2018@fh-salzburg.ac.at>
2020-04-29 16:41:21 +0200

    Make it accessible

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,12 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins '*'
-      resource '/*', headers: :any, methods: :get
+      resource '/*', headers: :any, methods: %i(get post put patch delete options head)
     end
    
     allow do
       origins 'localhost:3000', '127.0.0.1:3000', 'https://my-frontend.org'
-      resource '/*',
+      resource '/api/v1/*',
         methods: %i(get post put patch delete options head)
     end
   end
